e546299eec88047cd07b7038420742d24fd4a2f9
Kiko Fernandez <kikofernandezreyes@gmail.com>
2012-10-19 08:39:10 +0200

    now you can use functions as well, you can save data and functions because we have created a small façade

---
+++ templates.js
@@ -0,0 +1,51 @@
+var templates = function(){
+	var l = {};
+
+	function get(key){
+		return l[key];
+	}
+
+	function set(key, fun, opts){
+		l[key] = { func: fun, params: opts };
+	}
+
+	function remove(key){
+		console.log('not implemented');
+	}
+
+	function getParams(){
+
+	}
+
+	return {
+		get: get,
+		getParams: getParams,
+		set: set,
+		remove: remove,
+	}
+
+}();
+/*
+var t = (function(){ 
+	var t = function(){ 
+		return new t.fn.init();
+	}; 
+
+	t.fn = t.prototype = { 
+		constructor: t, 
+		init: function(){ 
+			console.log('init'); 
+		},
+
+		ja: function(){
+			console.log('ja');
+		},
+
+		jo: function(){
+			console.log('jo');
+		}
+	}; 
+
+	t.fn.init.prototype = t.fn; 
+})()
+*/
